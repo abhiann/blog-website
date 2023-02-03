a7604dcc06677eb91bf1df3124db220d498edd12
Abhishek Ranjan <abhishekrjnn@gmail.com>
2023-02-03 18:55:23 +0530

    add post to array

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const posts =[];
+
 app.get("/", function(req, res){
   res.render("home",{hsContent: homeStartingContent});
 });
@@ -32,8 +34,12 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postContent);
-  res.send();
+  const post ={
+    title: req.body.postTitle,
+    content: req.body.postContent
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
